b14449056c1f51aa89403cd01cfe946d62e0fb6f
Haggai C. Patrick <74777072+haeguy@users.noreply.github.com>
2022-04-07 15:21:06 +0100

    first commit

---
+++ js/jsIdentifiers.js
@@ -0,0 +1,10 @@
+let lastname, lastName;
+LastName = "Haggai";
+lastName = "Patrick";
+document.getElementById("name").innerHTML=LastName ;
+document.getElementById("changeh4").innerHTML="money sweet die";
+let x, y, z;
+x = 5;
+y = x + 5;
+z = " The answer is "
+document.getElementById("demo").innerHTML=z+y;
---
+++ js/jsOperators.js
@@ -0,0 +1,15 @@
+// let x, y;
+// x = 5;
+// y = 6;
+// document.getElementById("demo").innerHTML=x+y;
+var x, y;
+x = 5;
+y = 6;
+document.getElementById("demo").innerHTML=x+y;
+var z;
+z = 5;
+document.getElementById("demo2").innerHTML=z*10
+var g,h;
+g = 5 + 6;
+h = g*10;
+document.getElementById("demo3").innerHTML=g*10 
